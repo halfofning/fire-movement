c24e871abaa80db1ccdd4aced18f1ef6d87b8c09
Ning <wanninglim@gmail.com>
2021-06-28 19:20:19 +0800

    Improve player and ally movement

---
+++ Fire Movement/Assets/Scripts/AllyMovement.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.AI;
+
+public class AllyMovement : MonoBehaviour
+{
+    public Transform player;
+    public List<GameObject> allies;
+    public List<GameObject> enemies;
+    
+    public float speed;
+    public float targetDistance;
+    public float allowedDistance;
+    public RaycastHit hit;
+
+    private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
+    private KeywordRecognizer keywordRecognizer;
+
+    private List<string> spokenWords; // stores all the words spoken
+    private string ally = "";
+    private GameObject currAlly = null;
+
+    private int layerMask = 1 << 9;
+
+    private void Start()
+    {
+        // check which ally called
+        keywordActions.Add("first man", AllyChange);
+        keywordActions.Add("second man", AllyChange);
+        keywordActions.Add("third man", AllyChange);
+
+        keywordActions.Add("cover", Cover);
+        keywordActions.Add("scan", Scan);
+
+        keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
+        keywordRecognizer.Start();
+        Debug.Log(keywordRecognizer.IsRunning);
+
+        foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }
+    }
+
+    private void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
+    {
+        ally = args.text;
+        spokenWords.Add(args.text);
+        Debug.Log("Keyword: " + args.text);
+        keywordActions[args.text].Invoke();
+    }
+
+    private void AllyChange()
+    {
+        if (ally == "first")
+            currAlly = allies[0];
+        else
+            currAlly = allies[1];
+    }
+
+    private void Cover()
+    {
+        Debug.Log("cover");
+
+        currAlly.GetComponent<Animator>().Play("Firing Rifle");
+        currAlly = null;
+    }
+
+    private void Scan()
+    {
+        Debug.Log("scan");
+
+        currAlly.GetComponent<Animator>().Play("Scan");
+        currAlly = null;
+    }
+
+    void Update()
+    {
+        foreach (GameObject ally in allies)
+        {
+            // Make allies look at player
+            //Debug.Log(ally);
+            //Vector3 direction = player.transform.position - ally.transform.position;
+            //direction.y = 0;
+
+            //ally.transform.rotation = Quaternion.Slerp(ally.transform.rotation,
+            //                        Quaternion.LookRotation(direction), 0.1f);
+
+            //if (direction.magnitude > 5)
+            //{
+            //    ally.transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+            //    ally.GetComponent<Animator>().Play("Rifle Walk");
+            //} else
+            //{
+            //    speed = 0;
+            //    ally.GetComponent<Animator>().Play("Idle");
+            //}
+
+            // Ally look at player
+            ally.transform.LookAt(player.transform);
+
+            // Follow player
+            if (Physics.Raycast(ally.transform.position, ally.transform.TransformDirection(Vector3.forward), out hit, layerMask))
+            {
+                Vector3 direction = player.transform.position - ally.transform.position;
+                direction.y = 0;
+
+                Debug.DrawRay(ally.transform.position, ally.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                targetDistance = hit.distance;
+
+                if (targetDistance >= allowedDistance)
+                {
+                    speed = 0.015f;
+                    Debug.Log(ally + " run!");
+                    ally.GetComponent<Animator>().Play("Rifle Walk");
+                    ally.transform.position = Vector3.MoveTowards(ally.transform.position, player.transform.position, speed);
+                }
+                else
+                {
+                    Debug.Log(ally + " stop!");
+                    speed = 0;
+                    ally.GetComponent<Animator>().Play("Idle");
+                }
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (keywordRecognizer != null && keywordRecognizer.IsRunning)
+        {
+            keywordRecognizer.OnPhraseRecognized -= OnKeywordsRecognized;
+            keywordRecognizer.Stop();
+        }
+    }
+}
---
+++ Fire Movement/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 moveInput;
+    public SteamVR_Action_Single speedInput;
+    private CharacterController characterController;
+
+    public float speedMultiplier;
+    private float speed;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(moveInput.axis.x, 0, moveInput.axis.y));
+        speed = (float)(speedInput.axis * speedMultiplier);
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0, 9.81f, 0) * Time.deltaTime);
+    }
+}
